5d0d3e3d9800694532e3bd150c7d939b192daf08
helloecho12345 <92740628+helloecho12345@users.noreply.github.com>
2022-02-22 12:51:19 +0000

    added statement header and test

--- bank_tech_test/lib/bank_account.rb
+++ bank_tech_test/lib/bank_account.rb
@@ -2,4 +2,9 @@ class BankAccount
   def bank_account
     bank_account = BankAccount.new
   end
+
+  def statement_header
+    return "date || credit || debit || balance"
+  end
+
 end
--- bank_tech_test/spec/bank_account_spec.rb
+++ bank_tech_test/spec/bank_account_spec.rb
@@ -6,4 +6,8 @@ describe 'Bank Account' do
     expect(bank_account).to be_an_instance_of BankAccount
   end
 
+  it 'can print a statement header' do
+    bank_account = BankAccount.new
+    expect(bank_account.statement_header).to eq "date || credit || debit || balance"
+  end
 end
